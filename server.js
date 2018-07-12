c3a898a098134d96e74a66324c904a091eb6a34a
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2018-07-12 16:10:45 +0800

    configure package.json and server.js for heroku

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 
 hbs.registerHelper('getCurYear', () => new Date().getFullYear());
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', `${log}\n`, (err) => {
+        if (err)
+            throw err;
+    });
+
     next();
 });
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -41,6 +57,6 @@ app.get('/bad', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000.");
+app.listen(port, () => {
+    console.log(`Listening to port ${port}.`);
 });
